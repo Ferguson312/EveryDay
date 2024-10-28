8ac74723d96a473f2a1cbf988543becc4450dff4
Туренков Владислав <137440160+Ferguson312@users.noreply.github.com>
2024-10-28 11:01:57 +0500

    Каркас приложения

---
+++ obj/Debug/110/android/src/crc64bb1b55f618e11607/NotesActivity.java
@@ -0,0 +1,101 @@
+package crc64bb1b55f618e11607;
+
+
+public class NotesActivity
+	extends androidx.appcompat.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onBackPressed:()V:GetOnBackPressedHandler\n" +
+			"n_onCreateOptionsMenu:(Landroid/view/Menu;)Z:GetOnCreateOptionsMenu_Landroid_view_Menu_Handler\n" +
+			"n_onOptionsItemSelected:(Landroid/view/MenuItem;)Z:GetOnOptionsItemSelected_Landroid_view_MenuItem_Handler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"n_onNavigationItemSelected:(Landroid/view/MenuItem;)Z:GetOnNavigationItemSelected_Landroid_view_MenuItem_Handler:Google.Android.Material.Navigation.NavigationView/IOnNavigationItemSelectedListenerInvoker, Xamarin.Google.Android.Material\n" +
+			"";
+		mono.android.Runtime.register ("App2.NotesActivity, App2", NotesActivity.class, __md_methods);
+	}
+
+
+	public NotesActivity ()
+	{
+		super ();
+		if (getClass () == NotesActivity.class)
+			mono.android.TypeManager.Activate ("App2.NotesActivity, App2", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public NotesActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == NotesActivity.class)
+			mono.android.TypeManager.Activate ("App2.NotesActivity, App2", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onBackPressed ()
+	{
+		n_onBackPressed ();
+	}
+
+	private native void n_onBackPressed ();
+
+
+	public boolean onCreateOptionsMenu (android.view.Menu p0)
+	{
+		return n_onCreateOptionsMenu (p0);
+	}
+
+	private native boolean n_onCreateOptionsMenu (android.view.Menu p0);
+
+
+	public boolean onOptionsItemSelected (android.view.MenuItem p0)
+	{
+		return n_onOptionsItemSelected (p0);
+	}
+
+	private native boolean n_onOptionsItemSelected (android.view.MenuItem p0);
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+
+	public boolean onNavigationItemSelected (android.view.MenuItem p0)
+	{
+		return n_onNavigationItemSelected (p0);
+	}
+
+	private native boolean n_onNavigationItemSelected (android.view.MenuItem p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
